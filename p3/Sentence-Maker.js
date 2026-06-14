939802169ad4bb99b1d46f3fa71d8ad616a7897a
parsina33512 <themformr.psworks@gmail.com>
2026-06-15 00:45:43 +0330

    p3

---
+++ p3/Sentence-Maker.js
@@ -0,0 +1,18 @@
+let adjective="clever";
+let noun="adventurer";
+let verb="food-time";
+let place="KFC";
+let adjective2="seasoning";
+let noun2="fried-chicken";
+const firstStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("First story: "+firstStory);
+console.log(firstStory);
+
+adjective="dummy";
+noun="wonderer";
+verb="fast-time";
+place="desert";
+adjective2="sand";
+noun2="stones";
+const secondStory="Once upon a time, there was a(n) "+adjective+" "+noun+" who loved to eat "+noun2+". The "+noun+" lived in a "+place+" and had "+adjective2+" nostrils that blew fire when it was "+verb+".";
+console.log("Second story: "+secondStory);
